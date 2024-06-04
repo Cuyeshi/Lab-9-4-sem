764df6ba29f743a75db820196e673c9acc5b6da9
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-06-05 01:33:09 +0300

    Добавьте файлы проекта.

---
+++ Lab_9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main()
+        {
+            File.ReadAllLines("path/to/your/file.txt")
+                .Where(line => line.Split(' ').Any(word => DateTime.TryParse(word, out _)))
+                .SelectMany(line => line.Split(' ').Where(word => DateTime.TryParse(word, out _)).Select(word => DateTime.Parse(word)))
+                .OrderBy(date => date)
+                .ToList()
+                .ForEach(date => Console.WriteLine(date.ToString(CultureInfo.InvariantCulture)));
+        }
+    }
+}
